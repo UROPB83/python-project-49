c2f80879ea0c67ceb1641017e33912761a8f6ae4
UROPB83 <grimbraza@gmail.com>
2023-07-20 00:09:12 +0300

    add4

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/UROPB83/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/UROPB83/python-project-49/actions)
-https://asciinema.org/connect/bf52c79c-02de-4bc2-964d-966b57150c25
+brain-even:https://asciinema.org/connect/bf52c79c-02de-4bc2-964d-966b57150c25
+brain-calc:https://asciinema.org/connect/bf52c79c-02de-4bc2-964d-966b57150c25 
---
+++ brain_games/games/calc.py
@@ -0,0 +1,16 @@
+from random import choice, randint
+from operator import sub, add, mul
+
+RULES = 'What is result of the expression?'
+OPERATIONS = [("+", add), ("-", sub), ("*", mul)]
+
+
+def make_question_and_correct_answer():
+    a = randint(1, 20)
+    b = randint(1, 20)
+
+    operator, func = choice(OPERATIONS)
+
+    correct_answer = func(a, b)
+    question = f"{a} {operator} {b}"
+    return question, str(correct_answer)
