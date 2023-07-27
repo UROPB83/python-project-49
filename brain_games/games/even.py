c06759854398d3eaee70225ca1e27d98a6882716
UROPB83 <grimbraza@gmail.com>
2023-07-27 11:54:42 +0300

    add

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,6 +1,6 @@
 from random import randint
 
-RULES = 'Answer "yes" if the number even, otherwise answer "no".'
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 MIN_NUM = 1
 MAX_NUM = 999
 
